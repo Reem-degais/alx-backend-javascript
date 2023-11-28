b39cdf74521d6454460efe33b1bdf77f77cd882d
Reem-degais <reem.degais.33@gmail.com>
2023-11-28 02:31:11 +0000

    -

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,3 @@
 export default function returnHowManyArguments(...Args) {
-  let number_of_arguments = 0;
-
-  for (let arg of Args) {
-    number_of_arguments++
-  }
-
-  return number_of_arguments;
+  return Args.length;
 }
